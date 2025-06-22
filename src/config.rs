3f69b2c27ab756cca8fa85f18d6382b9250cdc90
William Doyle <ixm@ust.edu.kg>
2025-06-22 19:55:54 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -72,7 +72,7 @@ lazy_static::lazy_static! {
         let mut map = HashMap::new();
         map.insert("password".to_string(), "Uu123456*".to_string());
         RwLock::new(map)
-    };;
+    };
     pub static ref BUILTIN_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
 }
 
